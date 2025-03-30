8c047b9b72f340e8e9bbabb8d8f0e541544cb753
DownloadYourCourse <downloadyourcourse@gmail.com>
2025-03-30 11:37:57 +0000

    add security cors and helmet middleware to server.js (4929c7d) - auto-sync from master repo

--- src/server.js
+++ src/server.js
@@ -7,27 +7,28 @@ const port = process.env.PORT || 5000;                   //importing port value 
 const listingRoutes = require("./routes/listingRoutes");
 const serverHealthRoutes = require("./routes/serverHealth");
 
+// connectDB();
 
-// starting server
+// app.listen(port, () => console.log(`Server is running on port ${port}`));
+
 console.log('Server Initializing...')
+
+
+// Connect to the database first
+connectDB().then(() => {
+    // Start the server only after DB connection is successful
+    app.listen(port, () => console.log(`Server is running on port ${port}`));
+  }).catch(err => {
+    console.error("Server failed to start due to DB connection error", err);
+  });
+
 
 
 // Apply security middleware
 app.use(helmet());  // Protects against common security vulnerabilities
 app.use(cors());    // Enables Cross-Origin Resource Sharing
 
-
-// Connect to the database before handling requests
-connectDB()
-  .then(() => {
-    console.log("Connected to MongoDB");
-  })
-  .catch((err) => {
-    console.error("DB Connection Error:", err);
-  });
-
-
-//Body Parser Middlewares
+  //Body Parser Middlewares
 app.use(express.json());  // for raw jsons
 app.use(express.urlencoded({ extended: false})); // for form data
 
@@ -43,7 +44,3 @@ app.use('/api/listing', listingRoutes);
 
 //routes related to health of the server for load-balancing
 app.use('/api/health', serverHealthRoutes);
-
-
-// Firebase Cloud Function Export. ONLY RELVANT TO FIREBASE/ FIREBASE FUNCTIONS.
-exports.api = functions.https.onRequest(app);
