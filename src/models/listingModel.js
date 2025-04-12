cfac06c36e5297031ba48dd5d37f92770d4ed16a
DownloadYourCourse <downloadyourcourse@gmail.com>
2025-04-12 05:05:47 +0000

    add seoAuthor validation (4a14929) - auto-sync from master repo

--- src/models/listingModel.js
+++ src/models/listingModel.js
@@ -19,7 +19,13 @@ const listingSchema = new mongoose.Schema({
                     required: true
                 },
             }],
-            required: [true, 'At least one seoAuthor is required']
+            required: [true, 'At least one seoAuthor is required'],
+            validate: {
+                validator: function (v) {
+                  return Array.isArray(v) && v.length > 0;
+                },
+                message: 'seoAuthors must have at least one entry',
+              },
         },
         seoRobots: { type: String, enum: { values: ['index', 'noindex', 'follow', 'nofollow', 'index, follow', 'index, nofollow', 'noindex, follow', 'noindex, nofollow'], message: 'Invalid value for seoRobots. Must be small caps and one of: index, noindex, follow, nofollow, or their valid combinations.' } }, // noindex, no follow, index , follow for the search enginge robots crawlers and based on this tag they will crawl
         CanonicalUrl: { type: String, match: [/^https?:\/\/(www\.)?[\w\-]+(\.[\w\-]+)+([\/?#][^\s]*)?$/, 'Please provide a valid canonical URL. Example: https://yourwebsite.com/page'] }, // Tells search engines what the official URL of this content is, which helps prevent duplicate indexing. If this is the real content leave it blank or store your website url. Helpful when there are multiple domains pointing to the same content. Avoid Duplicate URLs Even your own site can serve the same content under multiple URLs accidentally:  https://downloadyourcourses.com/page, https://www.downloadyourcourses.com/page, https://downloadyourcourses.com/page?ref=abc. All technically show the same thing. Canonical URL tells Google: “Treat this one as the main version.” so the google only ranks the canonical url on search results and leave others. Use the full, clean, final URL: 'https://downloadyourcourses.com/currency-strength-meter'
