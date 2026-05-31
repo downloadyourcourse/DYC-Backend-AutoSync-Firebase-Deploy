aa6de82ab08198f88ada154a3f936af4201b2502
DownloadYourCourse <downloadyourcourse@gmail.com>
2026-05-31 09:44:08 +0000

    refactor: formatting and spacing (63b47dd) - auto-sync from master repo

--- src/controllers/listingControllers.js
+++ src/controllers/listingControllers.js
@@ -89,24 +89,24 @@ const createListing = async (req, res) => {
         // strict checking isPurchasableProduct === false because there are chances, isProductPurchasable is even not added to the
         // request body and this way it will be undefined but still the user might want to create a pricing document because
         // in mongoose its default value is set to true.
-        if(isPurchasableProduct === false){
+        if (isPurchasableProduct === false) {
             console.log('Pricing document will not be created and no reference will be given in listing document. isPurchasableProduct flag set to false.')
         }
-        else{
+        else {
             newPricingModelData = { listingId: req.body.listingId, listingTitle: listingTitle, ...pricing };  // only send the required data to pricing model and not the complete req body
             createdPricingDocument = await Pricing.create([newPricingModelData], { session });  // atomicity syntax
         }
 
         // 2️ Create Listing Document and add reference to the Pricing Document if createdPricingDocument is available or isPurchasableProduct is not false.
         let newListing; // created outside if block so that they are available in the complete code not just inside the if because let and const are block scoped.
-        if(createdPricingDocument){
+        if (createdPricingDocument) {
             const newListingData = { ...req.body, pricing: createdPricingDocument[0]._id }; // adding reference to Pricing document in the form of its id
             newListing = await Listing.create([newListingData], { session }); // atomicity syntax
         }
-        else{ // if else block runs, this means isProductPurchasable was false and hence we do not have pricingDocument and thus pricing reference will not be added. Applicable when it is a blog or related listing. Pricing is not set to required: true in mongoose listing schema. so its optional.
-            newListing = await Listing.create([ { ...req.body } ], { session }); // atomicity syntax
+        else { // if else block runs, this means isProductPurchasable was false and hence we do not have pricingDocument and thus pricing reference will not be added. Applicable when it is a blog or related listing. Pricing is not set to required: true in mongoose listing schema. so its optional.
+            newListing = await Listing.create([{ ...req.body }], { session }); // atomicity syntax
         }
-        
+
 
         // Commit the Transaction
         await session.commitTransaction();
@@ -175,8 +175,8 @@ const deleteListing = async (req, res) => {
         let deletedRefPricingDoc;
 
         // if pricing document does not exist inside the listing document (in case of a blog listing, it will not contain reference of pricing document). So avoiding findOneAndDelete for pricing document.
-        if(deletedListingDoc.pricing){
-            deletedRefPricingDoc = await Pricing.findOneAndDelete({_id: deletedListingDoc.pricing}, { session }); // atomicity syntax
+        if (deletedListingDoc.pricing) {
+            deletedRefPricingDoc = await Pricing.findOneAndDelete({ _id: deletedListingDoc.pricing }, { session }); // atomicity syntax
         }
 
         // Commit the Transaction
@@ -219,10 +219,10 @@ const updateListing = async (req, res) => {
         // listingId is immutable to avoid inconsistency in database
         if ('listingId' in req.body) {
             return res.status(400).json({
-              success: false,
-              message: "Updating 'listingId' is not allowed."
+                success: false,
+                message: "Updating 'listingId' is not allowed."
             });
-          }
+        }
 
         const { pricing, ...listingUpdateFields } = req.body; // Extract pricing and listing fields separately
         const pricingUpdateFields = pricing // for better readibility and maintainibility of code
@@ -233,10 +233,10 @@ const updateListing = async (req, res) => {
         // will automatically update in pricing
         if ('listingTitle' in pricingUpdateFields) {
             return res.status(400).json({
-              success: false,
-              message: "Updating 'listingTitle' directly in Pricing is not allowed."
+                success: false,
+                message: "Updating 'listingTitle' directly in Pricing is not allowed."
             });
-          }
+        }
 
         // query will be sent to the db server for querying and updating.
         // Will contain the id of listing/ pricing document and dot notation paths of fields to be updated with { $exists: true } to avoid
@@ -259,44 +259,44 @@ const updateListing = async (req, res) => {
         // updating/ patching all the individual fields of the raw json object. $set targets the individual fields for updates and do not replace the complete field in replacing based on the dot notation paths.
         const updatedListingDoc = await Listing.findOneAndUpdate(queryListing, { $set: dotNotationedListingUpdateFields }, { session, new: true, runValidators: true }); // atomicity syntax and 'new: true' ensures the updated document is returned instead of the original.
 
-        if(!updatedListingDoc){
+        if (!updatedListingDoc) {
             return res.status(404).json({ success: false, message: "Listing not found." })
         }
 
         let updatedPricingDoc;
 
         // additional check for updatedListingDoc.isPurchasableProduct, because if it was false then definitely its a blog or similar no sellable listing and it will not contain a pricing reference or document created for it. So avoiding the below code for unexpected behaviour.
-        if( (pricingUpdateFields && (Object.keys(pricingUpdateFields).length > 0) && updatedListingDoc.isPurchasableProduct ) || listingUpdateFields.listingTitle && updatedListingDoc.isPurchasableProduct ){
-            
+        if ((pricingUpdateFields && (Object.keys(pricingUpdateFields).length > 0) && updatedListingDoc.isPurchasableProduct) || listingUpdateFields.listingTitle && updatedListingDoc.isPurchasableProduct) {
+
             // assigning pricing doc id which is stored in the listing doc.
             queryPricing = { _id: updatedListingDoc.pricing }
 
             // the raw json object is converted to flattened dot notation paths with value to target invidual field instead of replacing the
             // complete field in nested documents.
-            const dotNotationedPricingUpdateFields = converToDotNotation({ listingTitle: updatedListingDoc.listingTitle, ...pricingUpdateFields});
-            
-            
+            const dotNotationedPricingUpdateFields = converToDotNotation({ listingTitle: updatedListingDoc.listingTitle, ...pricingUpdateFields });
+
+
             // upading the pricing document. $set targets the individual fields for updates and do not replace the complete field in replacing based on the dot notation paths.
             updatedPricingDoc = await Pricing.findOneAndUpdate(
                 queryPricing,
                 { $set: dotNotationedPricingUpdateFields },
                 { session, new: true, runValidators: true }) // atomicity syntax and 'new: true' ensures the updated document is returned instead of the original.
-            } else{
-                // check if updatedListingDoc.isPurchasableProduct is true, if it is false then no pricing document exists.
-                if( updatedListingDoc.isPurchasableProduct ) updatedPricingDoc = await Pricing.findOne({ _id: updatedListingDoc.pricing }, null, { session });
-            }
+        } else {
+            // check if updatedListingDoc.isPurchasableProduct is true, if it is false then no pricing document exists.
+            if (updatedListingDoc.isPurchasableProduct) updatedPricingDoc = await Pricing.findOne({ _id: updatedListingDoc.pricing }, null, { session });
+        }
 
-            // if the pricing doc is empty but the listing doc is found, it means something did not work as expected. But with a condition
-            // updatedListingDoc.isPurchasableProduct is true. That is the listing should have a pricing document as expected. because if its
-            // false, it is expected the pricing document was not created and hence will not be returned.
-            if(!updatedPricingDoc && updatedListingDoc.isPurchasableProduct){
-                throw new Error("Internal Server Error. Something went wrong. Aborting changes!");
-            }
+        // if the pricing doc is empty but the listing doc is found, it means something did not work as expected. But with a condition
+        // updatedListingDoc.isPurchasableProduct is true. That is the listing should have a pricing document as expected. because if its
+        // false, it is expected the pricing document was not created and hence will not be returned.
+        if (!updatedPricingDoc && updatedListingDoc.isPurchasableProduct) {
+            throw new Error("Internal Server Error. Something went wrong. Aborting changes!");
+        }
 
         // Commit the Transaction
         await session.commitTransaction();
 
-         // send response
+        // send response
         res.status(200).json({
             success: true,
             message: 'Listing and pricing details updated successfully.',
